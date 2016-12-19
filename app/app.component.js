aabba2fcfd4551cd12d3517d658931fad983eaed
Daniel Oram <danieloramnz@gmail.com>
2016-12-20 00:22:44 +1300

    add commenting

--- app/app.component.js
+++ app/app.component.js
@@ -8,3 +8,10 @@
       constructor: function() {}
     });
 })(window.app || (window.app = {}));
+
+
+//The Component method takes a configuration object with three properties.
+
+//The Class method is where we implement the component itself, 
+//giving it properties and methods that bind to the view and whatever 
+//behavior is appropriate for this part of the UI.
