6e0ff07421b3a05f786e989f741ee65dc9ed9602
Daniel Oram <danieloramnz@gmail.com>
2016-12-20 12:58:57 +1300

    add commenting

--- app/app.component.js
+++ app/app.component.js
@@ -1,5 +1,10 @@
+
+//Most application files export one thing by adding that thing to the app namespace. 
+//Our app.component.js file exports the AppComponent.
+
 (function(app) {
   app.AppComponent =
+  //When we need something from Angular, we use the ng object.
     ng.core.Component({
       selector: 'my-app',
       template: '<h1>Hello Angular</h1>'
@@ -8,6 +13,8 @@
       constructor: function() {}
     });
 })(window.app || (window.app = {}));
+//within each file we surround the code in an IIFE 
+//("Immediately Invoked Function Expression").
 
 
 //The Component method takes a configuration object with three properties.
