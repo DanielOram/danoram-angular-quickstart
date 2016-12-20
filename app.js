120308a346c99535cf9fb670b23f98bb348c05ec
Daniel Oram <danieloramnz@gmail.com>
2016-12-20 21:26:49 +1300

    add app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
+
+app.use( express.static(__dirname + '/client' ) );
